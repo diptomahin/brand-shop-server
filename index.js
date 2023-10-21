3a43551fe4f43027dbcb9984e73b6ab60d4553e5
Mahin Ahmed <mahindipto18@gmail.com>
2023-10-21 10:13:03 +0600

    added .env file and secured username and password

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port= process.env.PORT || 5000;
@@ -9,12 +10,10 @@ const port= process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//techFusion
-//kpek3YLrWYiQDJlb
 
 
-const uri = "mongodb+srv://techFusion:kpek3YLrWYiQDJlb@cluster0.pwyhut1.mongodb.net/?retryWrites=true&w=majority";
-console.log(uri);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pwyhut1.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
